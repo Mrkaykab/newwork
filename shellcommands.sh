e225c58f55d456751fc5ff218b94fcd4ac14b35d
mrkaykab <abankab@gmail.com>
2024-05-14 20:17:20 +0100

    new edits for assignment

--- shellcommands.sh
+++ shellcommands.sh
@@ -4,4 +4,8 @@ cat # to display the content of a file on terminal
 # more testing
 touch # to create file
 grep # to filter selection
+cd # to change directory
+sudo # for root access
+nano # to write to a file
+chmod # to change permission
 
