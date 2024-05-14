5e6543191df39f9dfc77598f696c2165ce50738c
mrkaykab <abankab@gmail.com>
2024-05-14 11:21:32 +0100

    more codes

--- shellcommands.sh
+++ shellcommands.sh
@@ -2,4 +2,6 @@ ls -la # to list content of a directory
 pwd # to print working directory
 cat # to display the content of a file on terminal
 # more testing
+touch # to create file
+grep # to filter selection
 
